33fd99177db3ae5e3194bc9e9f24ce9f5594cbe4
joindevops <info@joindevops.com>
2024-04-23 08:10:48 +0530

    functions

--- backend.sh
+++ backend.sh
@@ -30,7 +30,7 @@ unzip /tmp/backend.zip &>>$LOGFILE
 npm install &>>$LOGFILE
 
 #check your repo and path
-cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
+cp /home/ec2-user/expense-shell-1/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
 
 systemctl daemon-reload &>>$LOGFILE
 
