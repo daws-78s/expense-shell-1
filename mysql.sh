84af9dd6b0f7a02065a53c557f3312639fc1530a
joindevops <info@joindevops.com>
2024-04-23 08:04:06 +0530

    functions

--- mysql.sh
+++ mysql.sh
@@ -8,23 +8,16 @@ echo "Please enter DB password:"
 read -s mysql_root_password
 
 dnf install mysql-serddver -y &>>$LOGFILE
-#VALIDATE $? "Installing MySQL Server"
 
 systemctl enable mysqld &>>$LOGFILE
-#VALIDATE $? "Enabling MySQL Server"
 
 systemctl start mysqld &>>$LOGFILE
-#VALIDATE $? "Starting MySQL Server"
-
-# mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
-# VALIDATE $? "Setting up root password"
 
 #Below code will be useful for idempotent nature
 mysql -h db.daws78s.online -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
 if [ $? -ne 0 ]
 then
-    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
-    #VALIDATE $? "MySQL Root password Setup"
+    mysql_secure_infdfasstallation --set-root-pass ${mysql_root_password} &>>$LOGFILE
 else
     echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
 fi
